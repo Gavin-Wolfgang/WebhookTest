19b979e9b86a0ba22e3932c24359616e935d2a11
fuasbi <gavin32174@gmail.com>
2018-10-04 14:53:33 -0400

    started server

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, make_response, request, current_app, Request, jsonify
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/payload", methods=['POST'])
+def handlePost():
+	print(request)
+	print(request.json)
+	return "success"
+
+def main():
+	app.run(port=4567, debug=True)
+
+if __name__ == '__main__':
+	main()
